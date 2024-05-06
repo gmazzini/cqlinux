a0372ec3e910b42b9f3699f0a903e0f29a40a469
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-06 19:29:51 +0200

    Update q6.php

--- q6.php
+++ q6.php
@@ -36,8 +36,7 @@ foreach($cq as $k => $v){
   $ff=$aux[0]."_".$aux[1];
   echo $ff."\n";
   $aff=mktime(substr($ff,7,2),substr($ff,9,2),substr($ff,11,2),substr($ff,2,2),substr($ff,4,2),substr($ff,0,2));
-  // $sel[$k]=$vff-$aff;
-  $sel[$k]=1000/(30+$aux[2]);
+  $sel[$k]=($vff-$aff)+1000/(30+$aux[2]);
 }
 
 
