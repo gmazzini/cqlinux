0d98c15da3a01f0ff7c26da8c5785871de556b59
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-09 05:40:15 +0200

    Update cqsetup.php

--- cqsetup.php
+++ cqsetup.php
@@ -5,6 +5,16 @@ echo $gmwin."\n";
 shell_exec("import -silent -window $gmwin x1.tif");
 shell_exec("convert x1.tif -set colorspace Gray -separate -average x2.tif");
 shell_exec("tesseract x2.tif x3 hocr");
-
-
+$fp=fopen("x3.hocr","r");
+for(;;){
+  if(feof($fp))break;
+  $aux=fgets($fp);
+  if(strpos($aux,"<span")===false)continue;
+  $c1=strpos($aux,"title='");
+  if($c1===false)continue;
+  $c2=strpos($aux,"'",$c1+1);
+  $o1=substr($aux,$c1+7,$c2-$c1);
+  echo "$o1\n";
+}
+fclose($fp);
 ?>
