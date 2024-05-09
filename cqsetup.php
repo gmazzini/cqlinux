734533cc2c9b6966bbdfb19ce188f6ff8dbc9e42
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-09 07:28:47 +0200

    Update cqsetup.php

--- cqsetup.php
+++ cqsetup.php
@@ -55,9 +55,11 @@ for($i=0;$i<$con;$i++){
 }
 $ref=$on[$itop];
 $ctx=0;
-for($i=0;$i<$can;$i++)if($an[$i]["x1"]>$ref-5 && $an[$i]["x1"]<$ref+5 && $an[$i]["label"]<>"Now")$tx[$ctx++]=$i;
-if($i==$can){echo "Tx only $ctx\n"; exit(0);}
-  
+for($i=0;$i<$can;$i++)if($an[$i]["x1"]>$ref-5 && $an[$i]["x1"]<$ref+5 && $an[$i]["label"]<>"Now"){$txi[$ctx]=$i; $txv[$ctx]=$an[$i]["y1"]; $ctx++; }
+if($ctx<6){echo "Tx only $ctx\n"; exit(0);}
+array_multisort($tvx,$txi);
+for($i=0;$i<$ctx;$i++)printf("gmtx%d='%d %d'\n",$i,floor(($an[$txi[$i]]["x1"]+$an[$txi[$i]]["x2"])/2),floor(($an[$txi[$i]]["y1"]+$an[$txi[$i]]["y2"])/2));
+
 
 
 
