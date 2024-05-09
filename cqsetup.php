95fea17c27c2d388ed681591bfee75377ac393c1
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-09 05:32:39 +0200

    Update cqsetup.php

--- cqsetup.php
+++ cqsetup.php
@@ -1,6 +1,6 @@
 <?php
 
-$gmwin=shell_exec("xdotool search --onlyvisible --name 'K1JT'");
+$gmwin=trim(shell_exec("xdotool search --onlyvisible --name 'K1JT'"));
 echo $gmwin."\n";
 shell_exec("import -silent -window $gmwin x1.tif");
 shell_exec("convert x1.tif -set colorspace Gray -separate -average x2.tif");
