2610d72417418cb492c7aad46536bb19fe52febb
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-10 16:46:23 +0200

    Update cqsetup.php

--- cqsetup.php
+++ cqsetup.php
@@ -37,12 +37,12 @@ fclose($fp);
 for($i=0;$i<$can;$i++)if($an[$i]["label"]=="Enable")break;
 if($i==$can){echo "Enable not found\n"; exit(0);}
 $yb=$an[$i]["y2"];
-fprintf($fpw,"$gmenable='%d %d';\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
+fprintf($fpw,"\$gmenable='%d %d';\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 
 // Log
 for($i=0;$i<$can;$i++)if($an[$i]["label"]=="Log")break;
 if($i==$can){echo "Log not found\n"; exit(0);}
-fprintf($fpw,"$gmlog='%d %d';\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
+fprintf($fpw,"\$gmlog='%d %d';\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 $gmlog=sprintf("%d %d",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 
 // Tx
@@ -59,28 +59,28 @@ $ctx=0;
 for($i=0;$i<$can;$i++)if($an[$i]["x1"]>$ref-5 && $an[$i]["x1"]<$ref+5 && $an[$i]["label"]<>"Now"){$txi[$ctx]=$i; $txv[$ctx]=$an[$i]["y1"]; $ctx++; }
 if($ctx<6){echo "Tx only $ctx\n"; exit(0);}
 array_multisort($txv,$txi);
-for($i=0;$i<$ctx;$i++)fprintf($fpw,"$gmtx%d='%d %d';\n",$i+1,floor(($an[$txi[$i]]["x1"]+$an[$txi[$i]]["x2"])/2),floor(($an[$txi[$i]]["y1"]+$an[$txi[$i]]["y2"])/2));
+for($i=0;$i<$ctx;$i++)fprintf($fpw,"\$gmtx%d='%d %d';\n",$i+1,floor(($an[$txi[$i]]["x1"]+$an[$txi[$i]]["x2"])/2),floor(($an[$txi[$i]]["y1"]+$an[$txi[$i]]["y2"])/2));
 
 // Call
 for($i=0;$i<$can;$i++)if($an[$i]["label"]=="Call" && $an[$i]["y1"]>$yb)break;
 if($i==$can){echo "Call not found\n"; exit(0);}
-fprintf($fpw,"$gmcall='%d %d;'\n",2*$an[$i]["x1"]-$an[$i]["x2"],4*$an[$i]["y2"]-3*$an[$i]["y1"]);
+fprintf($fpw,"\$gmcall='%d %d;'\n",2*$an[$i]["x1"]-$an[$i]["x2"],4*$an[$i]["y2"]-3*$an[$i]["y1"]);
 
 // Report
 for($i=0;$i<$can;$i++)if($an[$i]["label"]=="Report" && $an[$i]["y1"]>$yb)break;
 if($i==$can){echo "Report not found\n"; exit(0);}
-fprintf($fpw,"$gmreport='%d %d;'\n",$an[$i]["x2"],floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
+fprintf($fpw,"\$gmreport='%d %d;'\n",$an[$i]["x2"],floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 $gmrxx=$an[$i]["x1"];
 
 // Rx
 for($i=$can-1;$i>=0;$i--)if($an[$i]["label"]=="Hz" && $an[$i]["y1"]>$yb)break;
 if($i<0){echo "Rx not found\n"; exit(0);}
-fprintf($fpw,"$gmrx='%d %d;'\n",$gmrxx,floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
+fprintf($fpw,"\$gmrx='%d %d;'\n",$gmrxx,floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 
 shell_exec("xdotool windowfocus --sync $gmwin mousemove --sync --window $gmwin $gmlog click 1");
 sleep(2);
 $gmlogwin=trim(shell_exec("xdotool search --onlyvisible --name 'Log'"));
-fprintf($fpw,"$gmlogwin=%s;\n",$gmlogwin);
+fprintf($fpw,"\$gmlogwin=%s;\n",$gmlogwin);
 shell_exec("import -silent -window $gmlogwin x5.tif");
 shell_exec("convert x5.tif -colorspace Gray -sharpen 0x1.0 x6.tif");
 shell_exec("tesseract x6.tif x7 --psm 6 hocr");
@@ -114,12 +114,12 @@ fclose($fp);
 // LogOK
 for($i=0;$i<$can;$i++)if($an[$i]["label"]=="OK")break;
 if($i==$can){echo "Log OK not found\n"; exit(0);}
-fprintf($fpw,"$gmlogok='%d %d;'\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
+fprintf($fpw,"\$gmlogok='%d %d;'\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 
 // LogCANCEL
 for($i=0;$i<$can;$i++)if($an[$i]["label"]=="Cancel")break;
 if($i==$can){echo "Log Cancel not found\n"; exit(0);}
-fprintf($fpw,"$gmlocancel='%d %d;'\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
+fprintf($fpw,"\$gmlocancel='%d %d;'\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 $gmlogcancel=sprintf("%d %d",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
 
 shell_exec("xdotool windowfocus --sync $gmlogwin mousemove --sync --window $gmlogwin $gmlogcancel click 1");
