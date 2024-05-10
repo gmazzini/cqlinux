52b9e35832de96b146a0fd7ee6ab5c6bba67255e
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-10 19:05:15 +0200

    Update cqrun.php

--- cqrun.php
+++ cqrun.php
@@ -7,19 +7,19 @@ for(;;){
   $aaout=trim(shell_exec("convert x11.tif -colorspace RGB -format '%[fx:mean.r] %[fx:mean.g] %[fx:mean.b]' info:"));
   $aux=explode(" ",$aaout); $aared=$aux[0]; $aagreen=$aux[1]; $aablu=$aux[2];
   if($aared<0.5 && $aagreen>0.1 && $aablu>0.1){
-    echo "Enable Off\n";
+    echo "STATUS: Enable Off\n";
     $aaout=shell_exec("xdotool windowfocus $gmlogwin 2>&1");
     if(strpos($aaout,"Bad")===false){
       sleep(6);
       shell_exec("xdotool windowfocus --sync $gmlogwin mousemove --sync --window $gmlogwin $gmlogok click 1");
-      echo "Log Ok\n";
+      echo "SET: Log Ok\n";
       sleep(6);      
     }
     shell_exec("xdotool windowfocus --sync $gmwin mousemove --sync --window $gmwin $gmtx6 click 1");
-    echo "Tx6\n";
+    echo "SET: Tx6\n";
     sleep(2);
     shell_exec("xdotool windowfocus --sync $gmwin mousemove --sync --window $gmwin $gmenable click 1");
-    echo "Enable On\n";
+    echo "SET: Enable On\n";
   }
   sleep(3);
 }
