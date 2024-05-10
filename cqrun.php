926efa138e6046535f721be4a8745f10c7c57736
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-10 07:21:17 +0200

    Update cqrun.php

--- cqrun.php
+++ cqrun.php
@@ -1,28 +1,23 @@
 <?php
+include "x4.php";
+$aux=explode(" ",$gmenable); $aax=$aux[0]; $aay=$aux[1];
+for(;;){
+  shell_exec("import -silent -window $gmwin x10.tif");
+  shell_exec("convert x10.tif -crop '10x10+$(($aax-5))+$(($aay-5))' +repage x11.tif");
+  $aaout=trim(shell_exec("convert x11.tif -colorspace RGB -format '%[fx:mean.r] %[fx:mean.g] %[fx:mean.b]' info:"));
+  $aux=explode(" ",$aaout); $aared=$aux[0]; $aagreen=$aux[1]; $aabu=$aux[2];
+  if($aared<0.5 && $aagreen>0.1 && $aablu>0.1){
+    $aaout=shell_exec("xdotool windowfocus $gmlogwin 2>&1");
+    if(strpos($aaout,"Bad")===false){
+      sleep(6);
+      shell_exec("xdotool windowfocus --sync $gmlogwin mousemove --sync --window $gmlogwin $gmlogok click 1");
+      sleep(6);      
+    }
+    shell_exec("xdotool windowfocus --sync $gmwin mousemove --sync --window $gmwin $gmtx6 click 1");
+    sleep(2);
+    shell_exec("xdotool windowfocus --sync $gmwin mousemove --sync --window $gmwin $gmenale click 1");
+  }
+  sleep(3);
+}
 
-source x4
-gmx=`echo $gmenable | awk -F" " '{print $1}'`
-gmy=`echo $gmenable | awk -F" " '{print $2}'`
-
-while true
-do
-  import -silent -window $gmwin x10.tif
-  convert x10.tif -crop "10x10+$(($gmx-5))+$(($gmy-5))" +repage x11.tif
-  aux=`convert x11.tif -colorspace RGB -format "%[fx:mean.r] %[fx:mean.g] %[fx:mean.b]" info:`
-  gmred=`echo $aux | awk -F" " '{print $1}'`
-  gmgreen=`echo $aux | awk -F" " '{print $2}'`
-  gmblu=`echo $aux | awk -F" " '{print $3}'`
-  if (( $(echo "$gmred<0.5"|bc -l) && $(echo "$gmgreen>0.1"|bc -l) && $(echo "$gmblu>0.1"|bc -l) )); then
-    aux=`xdotool windowfocus $gmlogwin 2>&1 | grep Bad | wc -l`
-    if (( ! $aux )); then
-      sleep 6
-      xdotool mousemove --window $gmlogwin $gmlogok click 1
-      sleep 6 
-    fi
-    xdotool mousemove --window $gmwin $gmtx6 click 1
-    sleep 2
-    xdotool mousemove --window $gmwin $gmenable click 1
-  fi
-  sleep 3
-done
-  ?>
+?>
