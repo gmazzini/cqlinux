2eabc33282a0a896b96e3f69c27e52b5b2eff97a
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-10 16:55:37 +0200

    Update cqrun.php

--- cqrun.php
+++ cqrun.php
@@ -1,9 +1,9 @@
 <?php
 include "x4.php";
-$aux=explode(" ",$gmenable); $aax=$aux[0]; $aay=$aux[1];
+$aux=explode(" ",$gmenable); $aax=$aux[0]-5; $aay=$aux[1]-5;
 for(;;){
   shell_exec("import -silent -window $gmwin x10.tif");
-  shell_exec("convert x10.tif -crop '10x10+$(($aax-5))+$(($aay-5))' +repage x11.tif");
+  shell_exec("convert x10.tif -crop '10x10+$aax+$aay' +repage x11.tif");
   $aaout=trim(shell_exec("convert x11.tif -colorspace RGB -format '%[fx:mean.r] %[fx:mean.g] %[fx:mean.b]' info:"));
   $aux=explode(" ",$aaout); $aared=$aux[0]; $aagreen=$aux[1]; $aabu=$aux[2];
   if($aared<0.5 && $aagreen>0.1 && $aablu>0.1){
