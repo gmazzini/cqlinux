5927ed98cf85bc092c05c0000f7678e339f49ace
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-11 05:39:15 +0200

    Update cqsetup.php

--- cqsetup.php
+++ cqsetup.php
@@ -35,7 +35,7 @@ for(;;){
 fclose($fp);
 
 // Enable
-for($i=0;$i<$can;$i++)if(substr($an[$i]["label"],0,-6)=="Enable")break;
+for($i=0;$i<$can;$i++)if($an[$i]["label"]=="Enable")break;
 if($i==$can){echo "Enable not found\n"; exit(0);}
 $yb=$an[$i]["y2"];
 fprintf($fpw,"\$gmenable='%d %d';\n",floor(($an[$i]["x1"]+$an[$i]["x2"])/2),floor(($an[$i]["y1"]+$an[$i]["y2"])/2));
