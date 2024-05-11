7ce05c6ae4daef2c8bbd88ab1febd982164c3f07
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-11 06:11:03 +0200

    Update q6.php

--- q6.php
+++ q6.php
@@ -33,26 +33,30 @@ foreach($cq as $k => $v){
 }
 
 // Scoring
-$ff=strftime("%y%m%d_%H%M%S");
-$vff=mktime(substr($ff,7,2),substr($ff,9,2),substr($ff,11,2),substr($ff,2,2),substr($ff,4,2),substr($ff,0,2));
-$x1lat=(ord(substr($g1,1,1))-65)*10+(int)substr($g1,3,1)+1/48-90;
-$x1lon=-((ord(substr($g1,0,1))-65)*20+(int)substr($g1,2,1)*2+1/24-180);
-$lat1=(float)$x1lat*M_PI/180;
-$lon1=(float)$x1lon*M_PI/180;
-foreach($cq as $k => $v){
-  $aux=explode("_",$v);
-  $ff=$aux[0]."_".$aux[1];
-  $aff=mktime(substr($ff,7,2),substr($ff,9,2),substr($ff,11,2),substr($ff,2,2),substr($ff,4,2),substr($ff,0,2));
-  $g2=$aux[4];
-  $x2lat=(ord(substr($g2,1,1))-65)*10+(int)substr($g2,3,1)+1/48-90;
-  $x2lon=-((ord(substr($g2,0,1))-65)*20+(int)substr($g2,2,1)*2+1/24-180);
-  $lat2=(float)$x2lat*M_PI/180;
-  $lon2=(float)$x2lon*M_PI/180;
-  $a=pow(sin(($lat1-$lat2)/2),2)+cos($lat1)*cos($lat2)*pow(sin(($lon1-$lon2)/2),2);
-  $dist=6371*2*atan2(sqrt($a),sqrt(1-$a));
-  $sel[$k]=($vff-$aff)+1000/(30+$aux[2])+100000/($dist+0.1);
+if(count($cq)>0){
+  $ff=strftime("%y%m%d_%H%M%S");
+  $vff=mktime(substr($ff,7,2),substr($ff,9,2),substr($ff,11,2),substr($ff,2,2),substr($ff,4,2),substr($ff,0,2));
+  $x1lat=(ord(substr($g1,1,1))-65)*10+(int)substr($g1,3,1)+1/48-90;
+  $x1lon=-((ord(substr($g1,0,1))-65)*20+(int)substr($g1,2,1)*2+1/24-180);
+  $lat1=(float)$x1lat*M_PI/180;
+  $lon1=(float)$x1lon*M_PI/180;
+  foreach($cq as $k => $v){
+    $aux=explode("_",$v);
+    $ff=$aux[0]."_".$aux[1];
+    $aff=mktime(substr($ff,7,2),substr($ff,9,2),substr($ff,11,2),substr($ff,2,2),substr($ff,4,2),substr($ff,0,2));
+    $g2=$aux[4];
+    $x2lat=(ord(substr($g2,1,1))-65)*10+(int)substr($g2,3,1)+1/48-90;
+    $x2lon=-((ord(substr($g2,0,1))-65)*20+(int)substr($g2,2,1)*2+1/24-180);
+    $lat2=(float)$x2lat*M_PI/180;
+    $lon2=(float)$x2lon*M_PI/180;
+    $a=pow(sin(($lat1-$lat2)/2),2)+cos($lat1)*cos($lat2)*pow(sin(($lon1-$lon2)/2),2);
+    $dist=6371*2*atan2(sqrt($a),sqrt(1-$a));
+    $sel[$k]=($vff-$aff)+1000/(30+$aux[2])+100000/($dist+0.1);
+  }
+  asort($sel);
 }
-asort($sel);
+
+
 
 $i=0;
 foreach($sel as $k => $v){
