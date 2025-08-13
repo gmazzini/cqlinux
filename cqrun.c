a4d83e1a8ccb2a7e1e1c935c693a7deff07a33f1
Gianluca Mazzini <gianluca@mazzini.org>
2025-08-13 06:41:37 +0200

    Update cqrun.c

--- cqrun.c
+++ cqrun.c
@@ -155,7 +155,7 @@ int main() {
       Ru32(&xx,&p);
       Rs(out,&p);
       Rs(out,&p);
-      if(transmitting)printf("@@@ %lu\n",(int)(((double)time(NULL))/7.5));
+      if(transmitting)printf("@@@ %lu\n",ceil(((double)time(NULL))/7.5));
       if((level&1) && (!enabletx) && (!mylock)){
         pthread_create(&thread,NULL,th_enabletx,NULL);
         pthread_detach(thread);
