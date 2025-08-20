da4db4a48e010249cc7fdfd8648eada5f9d85672
Gianluca Mazzini <gianluca@mazzini.org>
2025-08-20 05:37:59 +0200

    Update cqwhois.c

--- cqwhois.c
+++ cqwhois.c
@@ -51,6 +51,8 @@ void *whois_server_thread(){
         sprintf(out,"%d,%" PRIu32 ",%ld,%d,%3.1f,%" PRIu32 ",%s,%s,%d,%s,%" PRIu64 ",%d\n",i,rxed[i].ttime,rxed[i].time,rxed[i].snr,rxed[i].dt,rxed[i].df,rxed[i].mode,rxed[i].msg,rxed[i].LowConf,rxed[i].modeS,rxed[i].freqS,rxed[i].eoS);
         write(client_fd,out,strlen(out));
       }
+      sprintf(out,"# nrxed=%" PRIu32 "\n",nrxed);
+      write(client_fd,out,strlen(out));
     }
     else if(strcmp(ll,"cqed")==0){
       cqselection(selcall,&jsel,out);
