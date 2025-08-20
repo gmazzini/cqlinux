de2b75569f3ddada095d05f78b106eee3db4d46d
Gianluca Mazzini <gianluca@mazzini.org>
2025-08-20 07:24:52 +0200

    Update cqwhois.c

--- cqwhois.c
+++ cqwhois.c
@@ -77,7 +77,7 @@ void *whois_server_thread(){
       }
       for(busy[199]=2,j=200;j<=3000;j++){
         if(busy[j]==0 && busy[j]!=busy[j-1])e=j;
-        else if(busy[j]==1 && busy[j]!=busy[j-1]){sprintf(out,"%d-%s\n",e,j-1); write(client_fd,out,strlen(out)); }
+        else if(busy[j]==1 && busy[j]!=busy[j-1]){sprintf(out,"%d-%d\n",e,j-1); write(client_fd,out,strlen(out)); }
       }
     }
     else {
